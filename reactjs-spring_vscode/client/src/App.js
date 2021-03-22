c2e92a2855977b675465ba873712aa2e1e76c430
chinmay-anand <chinmay.subscriptions@gmail.com>
2021-03-23 03:04:46 +0530

    Applied styles to App.js

--- reactjs-spring_vscode/client/src/App.js
+++ reactjs-spring_vscode/client/src/App.js
@@ -7,8 +7,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Contacts />
+    <div className="container-fkuid">
+      <nav>
+        <div className="nav-wrapper center-align">
+          <a href="/" className="brand-logo">Contacts</a>
+        </div>
+      </nav>
+      <div className="row">
+        <Contacts />
+      </div>
     </div>
   );
 }
